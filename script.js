6788a0610c6a8e106400863ed1186739c66e4752
Bentura Peralez <bperalez001@gmail.com>
2021-09-24 23:24:01 -0500

    second upload

---
+++ script.js
@@ -0,0 +1,259 @@
+// Selectors 
+var timerArea = document.getElementById("timer-area");
+
+
+// Global variables 
+var timerInterval;
+var secondsRemaining = 5;
+
+
+// Functions 
+function startGame(){
+  var wordChosen = allTheWords[wordIdx];
+  // take the word chosen and make it into an array 
+  currentWordArr = wordChosen.split("");     //   ["t", "h", "o", "r"]
+  startTimer();
+  displayTheWord();
+}
+
+function startTimer(){
+  timerInterval = setInterval( function(){
+    timerArea.textContent = secondsRemaining + " seconds left";
+    secondsRemaining--;
+
+    if( secondsRemaining === 0 ){
+      clearInterval(timerInterval);
+      checkForWonGame();
+    }
+  }, 1000)
+}
+ 
+ 
+
+ const questions = [
+    {
+        question: "What is a string?",
+        optionA: "A group of numbers",
+        optionB: "A group of letters",
+        optionC: "A group of symbols",
+        optionD: "A group of characters written within quotation marks",
+        correctOption: "optionD"
+    },
+
+    
+
+    {
+        question: "What does HTML stand for ?",
+        optionA: "Higher Text Marking Language",
+        optionB: "Hyper Texile Marking Language",
+        optionC: "Hypertext Markup Language",
+        optionD: "None of the above",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "What is CSS used for?",
+        optionA: "It is used for the presentation of Web pages",
+        optionB: "It is used for the basic functionality of the Web pages",
+        optionC: "It is used to debug Web pages",
+        optionD: "It is used to add extra funtions to Web pages",
+        correctOption: "optionA"
+    },
+
+
+    {
+        question: "What is a flexbox layout?",
+        optionA: "provides a way for Web pages to be bolder",
+        optionB: "provides a way to graphically highlight images",
+        optionC: "provides a way to create a box that flexes in the Web page",
+        optionD: "provides a way to layout and move items in a container",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "what does javaScript do?",
+        optionA: "Design the website in a visually pleasing way",
+        optionB: "Implement features to make it more dynamic and interactive",
+        optionC: "Provides the basic foundation for a website to be built on",
+        optionD: "Creates the domain for the website",
+        correctOption: "optionB"
+    },
+
+
+
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
+
+function handleQuestions() { 
+    //function to shuffle and push 10 questions to shuffledQuestions array
+//app would be dealing with 10questions per session
+    while (shuffledQuestions.length <= 4) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
+let questionNumber = 1 //holds the current question number
+let playerScore = 0  //holds the player score
+let wrongAttempt = 0 //amount of wrong answers picked by player
+let indexNumber = 0 //will be used in displaying next question
+
+// function for displaying next question in the array to dom
+//also handles displaying players and quiz information to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+
+}
+
+
+
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "light green"
+            playerScore++ //adding to player's score
+            indexNumber++ //adding 1 to index so has to display next question..
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            wrongAttempt++ //adds 1 to wrong attempts 
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer() //check if player picked right or wrong option
+    unCheckRadioButtons()
+    //delays next question displaying for a second just for some effects so questions don't rush in on player
+    setTimeout(() => {
+        if (indexNumber <= 4) {
+//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+ 
+    const playerGrade = (playerScore / 5) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal, resets game and reshuffles questions
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
+
+let days = 2; //starting number of days
+let hours = 0; // starting number of hours
+let minutes = 2; // starting number of minutes
+let seconds = 5; // starting number of seconds
+
+// converts all to seconds
+let totalSeconds =
+days * 60 * 60 * 24 + hours * 60 * 60 + minutes * 60 + seconds;
+
+//temporary seconds holder
+let tempSeconds = totalSeconds;
+
+// calculates number of days, hours, minutes and seconds from a given number of seconds
+const convert = (value, inSeconds) => {
+if (value > inSeconds) {
+    let x = value % inSeconds;
+    tempSeconds = x;
+    return (value - x) / inSeconds;
+} else {
+    return 0;
+}
+};
+
